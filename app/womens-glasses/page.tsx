c569b2340a0e76649bceb49533e3d99256194afe
Roberto Leyva <robertoleyvaj@gmail.com>
2026-05-26 17:11:10 -0700

    Added TikTok Pixel

---
+++ app/womens-glasses/page.tsx
@@ -0,0 +1,10 @@
+import CollectionPage from "../components/CollectionPage";
+
+export default function Page() {
+  return (
+    <CollectionPage
+      title="Women's Glasses"
+      description="Stylish and affordable women's prescription glasses for everyday wear."
+    />
+  );
+}
